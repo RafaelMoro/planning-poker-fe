fb41ae6af88f419b35bfe14937d854362063b1e2
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-08-03 15:28:10 -0600

    Change: refactor room to send messages

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,33 +1,34 @@
-import { useContext, useEffect, useMemo, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import { RoomContext } from "../context/RoomContext";
 import { JOIN_ROOM_SOCKET } from "../constants";
 
 const Room = () => {
   const location = useLocation()
-  const users = useMemo(() => location?.state?.users ?? [], [location])
+  const [currentUser, setCurrentUser] = useState(location?.state?.user ?? '')
+  const [newUser, setNewUser] = useState('')
   const { ws } = useContext(RoomContext)
 
-  const [name, setName] = useState('')
   const { roomId } = useParams()
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setName(event.target.value)
+    setNewUser(event.target.value)
   }
   const handleUserJoin = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault()
+    setCurrentUser(newUser)
     if (ws) {
-      ws.emit(JOIN_ROOM_SOCKET, { roomId, userName: name })
+      ws.emit((roomId ?? JOIN_ROOM_SOCKET), { userName: currentUser, message: `${currentUser} joined the room` })
     }
   }
   
   useEffect(() => {
-    if (ws && users.length > 0) {
-      ws.emit(JOIN_ROOM_SOCKET, { roomId, userName: users[0] })
+    if (ws && currentUser) {
+      ws.emit((roomId ?? JOIN_ROOM_SOCKET), { userName: currentUser, message: `${currentUser} joined the room` })
     }
-  }, [roomId, ws, users])
+  }, [roomId, ws, currentUser])
 
-  if (users.length === 0) {
+  if (!currentUser) {
     return (
       <form action="submit">
         <input onChange={handleChange} type="text" placeholder="Your name" />
@@ -37,10 +38,12 @@ const Room = () => {
   }
 
   return (
-    <h1>
-      Welcome {users[0]}
+    <main>
+      <h1>
+        Welcome {currentUser}
+      </h1>
       <p>Join room: {`${window.origin}${location.pathname}`}</p>
-    </h1>
+    </main>
   )
 }
 
