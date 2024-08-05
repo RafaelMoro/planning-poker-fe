20db6fb5db98a4043cdb330233eb4e38be09f97f
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-08-05 00:21:03 -0600

    Add logic to create Room when room is created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import './App.css'
 import { Home } from './pages/Home'
 import { useEffect, useState } from 'react'
 import { socket } from './socket'
-import { CONNECT_EVENT, DISCONNECT_EVENT } from './constants'
+import { CONNECT_EVENT, DISCONNECT_EVENT, ROOM_CREATED_SOCKET } from './constants'
+import { User } from './interface'
 
 function App() {
+  const navigate = useNavigate()
   const [isConnected, setIsConnected] = useState(false)
 
   useEffect(() => {
@@ -17,14 +19,20 @@ function App() {
       setIsConnected(false)
     }
 
+    function handleRoomCreated({ roomId, newUser }: { roomId: string, newUser: User }) {
+      navigate(`room/${roomId}`, { state: { newUser } })
+    }
+
     socket.on(CONNECT_EVENT, onConnect)
     socket.on(DISCONNECT_EVENT, onDisconnect)
+    socket.on(ROOM_CREATED_SOCKET, handleRoomCreated)
 
     return () => {
       socket.off(CONNECT_EVENT, onConnect)
       socket.off(DISCONNECT_EVENT, onDisconnect)
+      socket.off(ROOM_CREATED_SOCKET, handleRoomCreated)
     }
-  }, [])
+  }, [navigate])
 
   useEffect(() => {
     socket.connect()
