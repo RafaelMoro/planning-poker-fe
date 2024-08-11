68973b1759da76512a5c76a36132a2d3b636453c
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-08-11 16:30:34 -0600

    Add space for show votes button and card names container

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -81,8 +81,8 @@ const Room = () => {
       { (users.length > 0) && (
         <>
           <h2 className="mt-8">Users:</h2>
-          { users.length > 0 && (<button className="mt-4" onClick={() => setShowVotes(true)}>Show votes</button>)}
-          <div className="flex flex-wrap gap-8">
+          { users.length > 0 && (<button className="my-4" onClick={() => setShowVotes(true)}>Show votes</button>)}
+          <div className="flex flex-wrap gap-8 mb-8">
             { users.map((user, index) => (
               <UserCard key={`${user.userName}-${index}`} user={user} showVote={showVotes} />
             ))}
