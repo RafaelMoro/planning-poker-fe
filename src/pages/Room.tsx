41335207c021eb22f5c6537b032056f1d426f577
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-08-11 16:26:08 -0600

    Add flex wrap

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -82,7 +82,7 @@ const Room = () => {
         <>
           <h2 className="mt-8">Users:</h2>
           { users.length > 0 && (<button className="mt-4" onClick={() => setShowVotes(true)}>Show votes</button>)}
-          <div className="flex gap-8">
+          <div className="flex flex-wrap gap-8">
             { users.map((user, index) => (
               <UserCard key={`${user.userName}-${index}`} user={user} showVote={showVotes} />
             ))}
